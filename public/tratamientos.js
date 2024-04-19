64aa87d901ccd3d73afe7fdbc1f648b4c782ca1c
Alejandro <alex.santamaria97@gmail.com>
2024-04-19 23:29:57 +0200

    arreglado hero

--- public/tratamientos.js
+++ public/tratamientos.js
@@ -3,6 +3,7 @@ export const tratamientosInfo = [
         id : 4,
         titulo : 'Presoterapia',
         descripcion : 'En nuestra clínica de fisioterapia, ofrecemos presoterapia, una técnica innovadora y no invasiva que utiliza la presión controlada para estimular la circulación sanguínea y linfática. La presoterapia es efectiva para reducir la retención de líquidos, aliviar la hinchazón, mejorar la circulación y promover la recuperación muscular. Durante las sesiones de presoterapia, los pacientes experimentan una sensación relajante y reconfortante mientras se benefician de los efectos revitalizantes de esta terapia. Ya sea como parte de un programa de rehabilitación integral o como un tratamiento independiente para mejorar la circulación y aliviar la fatiga, la presoterapia puede ser una adición beneficiosa a cualquier plan de cuidado de la salud. Nuestro equipo está comprometido a proporcionar una experiencia cómoda y segura para nuestros pacientes, asegurándonos de que se sientan revitalizados y rejuvenecidos después de cada sesión de presoterapia',
+        descripcionCorta : 'innovadora y no invasiva que utiliza la presión controlada para estimular la circulación sanguínea y linfática.',
         img : '../presoterapia.jpg'
     
     },
@@ -10,6 +11,7 @@ export const tratamientosInfo = [
         id : 3,
         titulo : 'Terapia manual',
         descripcion : 'En nuestra clínica de fisioterapia, reconocemos el valor y la eficacia de la terapia manual en el tratamiento de una variedad de condiciones musculoesqueléticas. Nuestro equipo de fisioterapeutas altamente calificados se especializa en terapia manual y está dedicado a proporcionar alivio del dolor, restauración de la movilidad y mejora de la función a través de técnicas manuales avanzadas y personalizadas. Desde masajes terapéuticos profundos hasta manipulaciones articulares suaves y estiramientos específicos, diseñamos planes de tratamiento individualizados que se adaptan a las necesidades únicas de cada paciente. Nuestro enfoque centrado en el paciente y nuestra atención meticulosa a los detalles nos permiten abordar eficazmente una amplia gama de problemas musculoesqueléticos, incluyendo contracturas, rigidez, limitaciones de movimiento y más. Nos esforzamos por empoderar a nuestros pacientes con las herramientas y técnicas necesarias para mejorar su bienestar y disfrutar de una vida activa y sin dolor',
+        descripcionCorta : 'innovadora y no invasiva que utiliza la presión controlada para estimular la circulación sanguínea y linfática.',
         img : '../presoterapia.jpg'
 
     },
@@ -17,6 +19,7 @@ export const tratamientosInfo = [
         id : 1,
         titulo : 'Fisioterapia deportiva',
         descripcion : 'En nuestra clínica de fisioterapia, nos enorgullece ofrecer un servicio especializado en fisioterapia deportiva diseñado para atender las necesidades únicas de los atletas de todos los niveles. Reconocemos que el rendimiento y la salud son fundamentales en el ámbito deportivo, por lo que nuestro equipo de fisioterapeutas deportivos altamente capacitados está comprometido a ayudar a los deportistas a alcanzar sus objetivos. Ya sea que estén recuperándose de una lesión, buscando mejorar su rendimiento o necesitando prevención de lesiones, nuestros profesionales trabajan de cerca con cada paciente para desarrollar un plan de tratamiento personalizado que aborde sus necesidades específicas. Utilizamos técnicas avanzadas, tecnología de punta y un enfoque holístico para optimizar la recuperación, promover la salud muscular y articular, y minimizar el riesgo de futuras lesiones. Desde el diagnóstico inicial hasta la rehabilitación completa, estamos dedicados a brindar atención integral y de alta calidad que permita a los deportistas volver a la acción con confianza y fuerza',
+        descripcionCorta : 'innovadora y no invasiva que utiliza la presión controlada para estimular la circulación sanguínea y linfática.',
         img : '../presoterapia.jpg'
 
     },
@@ -24,6 +27,24 @@ export const tratamientosInfo = [
         id : 2,
         titulo : 'Recuperación de lesiones',
         descripcion : 'En nuestra clínica de fisioterapia, comprendemos los desafíos físicos y emocionales que conlleva la recuperación de una lesión. Ya sea que se trate de una lesión deportiva, un accidente automovilístico o cualquier otra causa, nuestro equipo de fisioterapeutas especializados está aquí para ayudar a nuestros pacientes a superar el dolor, restaurar la movilidad y recuperar la funcionalidad perdida. Nos comprometemos a proporcionar un enfoque integral y personalizado para la recuperación de lesiones, que incluye evaluación detallada, tratamiento individualizado y seguimiento continuo para garantizar resultados óptimos. Nuestros fisioterapeutas altamente capacitados utilizan una variedad de técnicas avanzadas, terapias manuales, ejercicios terapéuticos y modalidades de rehabilitación para acelerar el proceso de curación y minimizar el riesgo de recaídas. Nos esforzamos por brindar un ambiente de apoyo y comprensión donde nuestros pacientes se sientan seguros y motivados para lograr una recuperación completa y una mejor calidad de vida',
+        descripcionCorta : 'innovadora y no invasiva que utiliza la presión controlada para estimular la circulación sanguínea y linfática.',
+        img : '../indiba.jpg'
+
+    },
+    {
+        id : 5,
+        titulo : 'Indiba',
+        descripcion : 'En nuestra clínica de fisioterapia, comprendemos los desafíos físicos y emocionales que conlleva la recuperación de una lesión. Ya sea que se trate de una lesión deportiva, un accidente automovilístico o cualquier otra causa, nuestro equipo de fisioterapeutas especializados está aquí para ayudar a nuestros pacientes a superar el dolor, restaurar la movilidad y recuperar la funcionalidad perdida. Nos comprometemos a proporcionar un enfoque integral y personalizado para la recuperación de lesiones, que incluye evaluación detallada, tratamiento individualizado y seguimiento continuo para garantizar resultados óptimos. Nuestros fisioterapeutas altamente capacitados utilizan una variedad de técnicas avanzadas, terapias manuales, ejercicios terapéuticos y modalidades de rehabilitación para acelerar el proceso de curación y minimizar el riesgo de recaídas. Nos esforzamos por brindar un ambiente de apoyo y comprensión donde nuestros pacientes se sientan seguros y motivados para lograr una recuperación completa y una mejor calidad de vida',
+        descripcionCorta : 'innovadora y no invasiva que utiliza la presión controlada para estimular la circulación sanguínea y linfática.',
+        img : '../indiba.jpg'
+
+    },
+    {
+        id : 6,
+        titulo : 'Ejercicio terapéutico',
+        descripcion : 'En nuestra clínica de fisioterapia, comprendemos los desafíos físicos y emocionales que conlleva la recuperación de una lesión. Ya sea que se trate de una lesión deportiva, un accidente automovilístico o cualquier otra causa, nuestro equipo de fisioterapeutas especializados está aquí para ayudar a nuestros pacientes a superar el dolor, restaurar la movilidad y recuperar la funcionalidad perdida. Nos comprometemos a proporcionar un enfoque integral y personalizado para la recuperación de lesiones, que incluye evaluación detallada, tratamiento individualizado y seguimiento continuo para garantizar resultados óptimos. Nuestros fisioterapeutas altamente capacitados utilizan una variedad de técnicas avanzadas, terapias manuales, ejercicios terapéuticos y modalidades de rehabilitación para acelerar el proceso de curación y minimizar el riesgo de recaídas. Nos esforzamos por brindar un ambiente de apoyo y comprensión donde nuestros pacientes se sientan seguros y motivados para lograr una recuperación completa y una mejor calidad de vida',
+        descripcionCorta : 'innovadora y no invasiva que utiliza la presión controlada para estimular la circulación sanguínea y linfática.',
+
         img : '../indiba.jpg'
 
     },
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,11 +8,11 @@ export default function Navbar() {
   return (
     <nav style={{justifyContent: 'space-between', backgroundColor: '#0c0c1d'}} className="flex py-6 items-center relative px-16 rounded-2xl">
 	<span className="text-2xl text-white ">PHYSIOENFORMA</span>
-	<ul style={{backgroundColor : '#0c0c1d'}} className={`${nav ? ' transition-opacity opacity-100' : ' transition-opacity duration-700 opacity-0 hidden'} duration-700 fixed z-10 top-0 w-full h-screen left-0 items-center text-2xl justify-center gap-12 flex flex-col md:flex-row md:static md:h-full md:w-auto md:bg-transparent md:text-lg md:gap-4 md:flex md:opacity-100`}>
+	<ul style={{backgroundColor : '#0c0c1d'}} className={`${nav ? ' transition-opacity opacity-100' : ' transition-opacity duration-700 opacity-0 hidden'} duration-700 fixed z-20 top-0 w-full h-screen left-0 items-center text-2xl justify-center gap-12 flex flex-col md:flex-row md:static md:h-full md:w-auto md:bg-transparent md:text-lg md:gap-4 md:flex md:opacity-100`}>
 		<li className="text-white hover:cursor-pointer hover:text-blue-400 transition duration-300"><a href="/">HOME</a></li>
 		<li className="text-white hover:cursor-pointer hover:text-blue-400 transition duration-300"><a href="/nosotros">NOSOTROS</a> </li>
-		<li className="text-white hover:cursor-pointer hover:text-blue-400 transition duration-300"><a href="#contacto">SERVICIOS</a></li>
-		<li className="text-white hover:cursor-pointer hover:text-blue-400 transition duration-300"><a href="#contacto">CONTACTO</a></li>
+		<li className="text-white hover:cursor-pointer hover:text-blue-400 transition duration-300"><a href="/#servicios">SERVICIOS</a></li>
+		<li className="text-white hover:cursor-pointer hover:text-blue-400 transition duration-300"><a href="/#contacto">CONTACTO</a></li>
 	</ul>
     {
         

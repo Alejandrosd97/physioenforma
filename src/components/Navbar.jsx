976bda807c042562ef67a4092421d8eae0e82cdf
Alejandro <alex.santamaria97@gmail.com>
2024-05-07 14:11:11 +0200

    animacion navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { useState } from 'react'
+import '../css/navbar.css'
 
 export default function Navbar() {
 
     const [nav, setNav] = useState(false)
 
   return (
-    <nav style={{justifyContent: 'space-between', backgroundColor: '#0c0c1d'}} className="flex py-6 items-center relative px-16 rounded-2xl">
+  
+    <nav style={{justifyContent: 'space-between', backgroundColor: '#0c0c1d'}} className=" anim flex py-6 sticky top-0 z-50 items-center relative px-16 rounded-2xl">
 	<span className="text-2xl text-white ">PHYSIOENFORMA</span>
-	<ul style={{backgroundColor : '#0c0c1d'}} className={`${nav ? ' transition-opacity opacity-100' : ' transition-opacity duration-700 opacity-0 hidden'} duration-700 fixed z-20 top-0 w-full h-screen left-0 items-center text-2xl justify-center gap-12 flex flex-col md:flex-row md:static md:h-full md:w-auto md:bg-transparent md:text-lg md:gap-4 md:flex md:opacity-100`}>
+	<ul style={{backgroundColor : '#0c0c1d'}} className={`${nav ? ' transition-opacity opacity-100' : 'textAnim transition-opacity duration-700 opacity-0 hidden'} duration-700 fixed z-20 top-0 w-full h-screen left-0 items-center text-2xl justify-center gap-12 flex flex-col md:flex-row md:static md:h-full md:w-auto md:bg-transparent md:text-lg md:gap-4 md:flex md:opacity-100`}>
 		<li className="text-white hover:cursor-pointer hover:text-blue-400 transition duration-300"><a href="/">HOME</a></li>
 		<li className="text-white hover:cursor-pointer hover:text-blue-400 transition duration-300"><a href="/nosotros">NOSOTROS</a> </li>
 		<li className="text-white hover:cursor-pointer hover:text-blue-400 transition duration-300"><a href="/#servicios">SERVICIOS</a></li>
